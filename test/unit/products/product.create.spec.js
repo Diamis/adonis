9cabf06e4f4e7dc718d5306e3a3eaa370f2e5cd1
Aleksey Chernyshov <alexey.chernyshov@distate.ru>
2020-11-16 11:21:42 +0300

    Product test create name, description

---
+++ test/unit/products/product.create.spec.js
@@ -0,0 +1,16 @@
+"use strict";
+
+const { test, trait } = use("Test/Suite")("Product Created");
+const ProductService = use("App/Core/Products/Services/ProductService");
+
+trait("DatabaseTransactions");
+
+test("append properties of name and description", async() => {
+  const service = new ProductService();
+  const product = await service.create({ 
+    name: 'product_name', 
+    description: 'product description'
+  });
+
+  console.log(product);
+})
