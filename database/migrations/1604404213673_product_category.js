053ff24dc22d0969544eed7e027a095ee47203f8
Aleksey Chernyshov <alexey.chernyshov@distate.ru>
2020-11-17 09:43:45 +0300

    migration append product_categories

---
+++ database/migrations/1604404213673_product_category.js
@@ -0,0 +1,26 @@
+'use strict'
+ 
+const Schema = use('Schema')
+
+class ProductCategory extends Schema {
+  up () {
+    this.create('product_categories', (table) => {
+      table.insert("product_id").unsigned();
+      table.foreign("product_id").references("id").inTable("products");
+
+      table.insert("category_id").unsigned();
+      table.foreign("category_id").references("id").inTable("categories");
+    })
+  }
+
+  down () {
+    this.table('product_categories', (table) => {
+      table.dropForeign("product_id");
+      table.dropForeign("category_id");
+    });
+    
+    this.drop('product_categories')
+  }
+}
+
+module.exports = ProductCategory 
